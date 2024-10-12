4ad12b176cc0d07575bb027f7c18d0378f4933d1
Toru Yamanaka <toruymnk@gmail.com>
2024-10-12 16:24:35 +0900

    [project 4] Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,56 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+    @SCREEN
+    D=A
+    @8192
+    D=D+A
+    @R0  // R0 holds the terminal address of the screen
+    M=D
+(LOOP)
+    @KBD
+    D=M
+    @FILL
+    D;JGT
+    @CLEAR
+    0;JMP
+(FILL)
+    @SCREEN
+    D=A
+    @R1
+    M=D
+(FILL_LOOP)
+    @R2
+    A=D
+    M=-1
+    @R1
+    M=M+1
+    D=M
+    @R0
+    D=M-D
+    @LOOP
+    D;JLE
+    @R1
+    D=M
+    @FILL_LOOP
+    0;JMP
+(CLEAR)
+    @SCREEN
+    D=A
+    @R1
+    M=D
+(CLEAR_LOOP)
+    @R2
+    A=D
+    M=0
+    @R1
+    M=M+1
+    D=M
+    @R0
+    D=M-D
+    @LOOP
+    D;JLE
+    @R1
+    D=M
+    @CLEAR_LOOP
+    0;JMP
